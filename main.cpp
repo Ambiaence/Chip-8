c8b52c1e14dfb6427ed2a7b379579da1dfb3ac61
George Kent Allen <georgekentallen@gmail.com>
2022-01-25 21:05:06 -0700

    Initilized registers and memory. A subset of the characters in standard output will be initalized into the 4kb memory.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
-//This is a change
-int main 
+
+unsigned char charToFourBitWord(unsigned char byte) //Takes a character and retuns a four bit word at the least sigificant bits of character
+{				
+	if(byte >= 'A' && byte <= 'F')
+		return byte - 'A' + 10 ;
+	else if(byte >= '0' && byte <= '9')
+		return byte - '0';
+	else return 'f'; //Returns a failure of the data was not within the correct range
+}
+
+int main() 
 {
-	reg char[16]; 
-	mem char[4096];
-	
-			
+	unsigned char reg[16] = {0, 0, 0, 0,
+			0, 0, 0, 0,
+			0, 0, 0, 0,  
+			0, 0, 0, 0};
+
+	unsigned char mem[4096];
+
+	unsigned char charOne; //Current characcter to be parsed into a 4 bit word
+	unsigned char charTwo; //Current characcter to be parsed into a 4 bit word
 		
+	for(int i = 0; i < 4096 &&  ; i++) {
+		if((charOne = charToFourBitWord(charOne)) == 'f'){
+			std::cout << "BYTE MUST BE A HEX VALUE" << std::endl;
+			return -1;
+		}
+			
+		if((charTwo = charToFourBitWord(charTwo)) == 'f') {
+			std::cout << "BYTE MUST BE A HEX VALUE" << std::endl;
+			return -1;
+		}
+		mem[i] = (charOne << 4) | charTwo;
+
+		std::cout << "Output was: " <<  (int)mem[i] << std::endl;
+	}
+		
+							
+
 }
 
